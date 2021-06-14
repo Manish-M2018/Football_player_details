79c38e2f76d408c9d61f0b64406bfaef9a38e3d6
Kaustubha DS <dskaustubh@gmail.com>
2021-06-14 22:45:44 +0530

    add sort and filter routes

--- Backend/index.js
+++ Backend/index.js
@@ -169,6 +169,69 @@ app.post('/delete_player', function (req, res) {
         });
 });
 
+
+
+app.post('/sort_attribute', function (req, res) {
+
+    /*
+        Input: attribue to be sorted, eg:{attribute: goals}
+
+        Output: list of player objects sorted based on goals
+    */
+
+    var attribute = req.fields.attribute;
+
+    footballer.find().then((dataArr) => {
+        return dataArr.sort((data1, data2) => {
+            if (data1[attribute] > data2[attribute]) {
+                return 1
+            }
+            else if (data1[attribute] === data2[attribute]) {
+                return 0
+
+            }
+            else {
+                return -1
+            }
+        })
+    }).then((dataArr) => {
+        res.json({
+            data: dataArr
+        })
+    }).catch((e) => {
+        console.log(e)
+    })
+});
+
+
+app.post("/filter_attribute", function (req, res) {
+
+    /*
+        Input: attribue and value
+
+        output: list of player satisying the filter
+    */
+
+    var attribute = req.fields.attribute;
+    var key = req.fields.key;
+
+
+    var temp_obj = {};
+
+    temp_obj[attribute]= key;
+    
+    footballer.find(temp_obj,function (err, data) {
+
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json({ data });
+        }
+    });
+
+});
+
 app.listen(3000, () => {
     console.log("Server running at port 3000");
 })
